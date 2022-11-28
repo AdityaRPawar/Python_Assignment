d8cb205d2882f8156c138318b1a50f9772111e76
AdityaRPawar <89921055+AdityaRPawar@users.noreply.github.com>
2022-11-28 18:11:56 +0530

    Add files via upload

---
+++ Python Assignment 02.py
@@ -0,0 +1,10 @@
+# Python program to find the smallest and largest number in a list
+
+lst = []
+num = int(input('How many numbers: '))
+
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+
+print("Maximum element in the list is :", max(lst), "\nMinimum element in the list is :", min(lst))
